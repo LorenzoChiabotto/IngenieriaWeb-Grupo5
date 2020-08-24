d76f71adf975046fbe145236821a3f70e7a3386d
Lorenzo Chiabotto <lorenzochiabotto@gmail.com>
2020-08-24 00:13:53 -0300

    password format to password fields

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,8 +4,8 @@ from django.core.exceptions import ValidationError
 class SignUp(forms.Form):
     user = forms.CharField(max_length=20)
     email = forms.EmailField(required=True)
-    password = forms.CharField(max_length=20)
-    confirm_password = forms.CharField(max_length=20)
+    password = forms.CharField(max_length=20,widget=forms.PasswordInput)
+    confirm_password = forms.CharField(max_length=20,widget=forms.PasswordInput)
 
     def clean_confirm_password(self):
         password = self.cleaned_data['password']
@@ -18,4 +18,4 @@ class SignUp(forms.Form):
 
 class Login(forms.Form):
     user = forms.CharField(max_length=50)
-    password = forms.CharField(max_length=20)
+    password = forms.CharField(max_length=20,widget=forms.PasswordInput)
