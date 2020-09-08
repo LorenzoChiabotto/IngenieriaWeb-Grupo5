c3ebb2cdd598a5aa32a40beefd6410881b4009a4
Manumusso <manumusso05@gmail.com>
2020-09-08 13:38:12 +0100

    models

--- webChat/settings.py
+++ webChat/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog.apps.CatalogConfig',
+    'django_extensions',
     'captcha'
-
 ]
 ACCOUNT_ACTIVATION_DAYS = 7
 MIDDLEWARE = [
