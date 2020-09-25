51a7735f0245e0807d0e0a47dd2f154db409ec25
Lorenzo Chiabotto <lorenzochiabotto@gmail.com>
2020-09-25 01:12:37 -0300

    updated references

--- chatRoom/urls.py
+++ chatRoom/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('render_message', views.render_message,  name='render_message'),
     path('render_user_message', views.render_user_message,  name='render_user_message'),
     path('rooms/list',views.chat_rooms, name='roomsList'),
-    path('rooms/create', views.create_chat_room, name='create_chat_room'),
+    path('rooms/create', views.create_chat_room, name='roomsCreate'),
     path('rooms/<str:room_pk>', views.room,  name='room'),
 
 ]
