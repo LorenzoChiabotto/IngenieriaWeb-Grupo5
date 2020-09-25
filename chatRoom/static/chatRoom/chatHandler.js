bfbe69f0858926dcb8cfa647e41daec2f045734a
Lorenzo Chiabotto <lorenzochiabotto@gmail.com>
2020-09-24 23:44:40 -0300

    room handle kick

--- chatRoom/static/chatRoom/chatHandler.js
+++ chatRoom/static/chatRoom/chatHandler.js
@@ -1,12 +1,12 @@
 
 var req = new XMLHttpRequest(); 
-req.open('GET', 'http://127.0.0.1:8000/chat/render_message', false); 
+req.open('GET', 'chat/render_message', false); 
 req.send({'chat':1});
 const message = req.response
 
 
 req = new XMLHttpRequest(); 
-req.open('GET', 'http://127.0.0.1:8000/chat/render_user_message', false); 
+req.open('GET', '/chat/render_user_message', false); 
 req.send({'chat':1});
 const user_message = req.response
 
@@ -28,18 +28,29 @@ const chatSocket = new WebSocket(
 
 chatSocket.onmessage = function (e) {
     const data = JSON.parse(e.data);
-    
-    var x = document.createElement('div')
-    if(document.getElementById('user_id').textContent == data.userId){
-        x.style.display= 'flex'
-        x.style.justifyContent = 'flex-end'
-        x.innerHTML = user_message
-    }else{
-        x.innerHTML = message
+    switch (data.type) {
+        case "chat_message":
+            var x = document.createElement('div')
+            if(document.getElementById('user_id').textContent == data.userId){
+                x.style.display= 'flex'
+                x.style.justifyContent = 'flex-end'
+                x.innerHTML = user_message
+            }else{
+                x.innerHTML = message
+            }
+            x.innerHTML = x.innerHTML.replace('__user__', data.userName)
+            x.innerHTML = x.innerHTML.replace('__messagge__', data.message)
+            document.querySelector('#chat-log').appendChild(x)
+            break;
+        case "chat_kick":
+            if(document.getElementById('user_id').textContent == data.userId){
+                    alert("You have been kicked out: "+ data.message)
+                    window.location.replace("/");
+            }
+        default:
+            break;
     }
-    x.innerHTML = x.innerHTML.replace('__user__', data.userName)
-    x.innerHTML = x.innerHTML.replace('__messagge__', data.message)
-    document.querySelector('#chat-log').appendChild(x)
+    
 
 };
 
@@ -57,6 +68,9 @@ document.querySelector('#chat-message-input').onkeyup = function (e) {
 document.querySelector('#chat-message-submit').onclick = function (e) {
     const messageInputDom = document.querySelector('#chat-message-input');
     const message = messageInputDom.value;
+    if (message === ""){
+        return
+    }
     chatSocket.send(JSON.stringify({
         'type': 'chat_message',
         'message': message,
