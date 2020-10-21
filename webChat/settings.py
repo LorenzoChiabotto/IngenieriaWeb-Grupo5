366060f6387beb3468c5d105eab1759d5019ae5d
Lorenzo Chiabotto <lorenzochiabotto@gmail.com>
2020-10-20 21:20:51 -0300

    added templates base dir

--- webChat/settings.py
+++ webChat/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'webChat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'catalog/templates'), os.path.join(BASE_DIR, 'chatRoom/templates'), ],
+        'DIRS': [os.path.join(BASE_DIR, 'catalog/templates'), os.path.join(BASE_DIR, 'chatRoom/templates'), os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [ 
