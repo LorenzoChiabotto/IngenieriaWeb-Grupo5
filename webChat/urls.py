716b126fedd6ee8277294757d4d17849d2cf8c1f
Lorenzo Chiabotto <lorenzochiabotto@gmail.com>
2020-10-20 21:19:53 -0300

    robots fixed

--- webChat/urls.py
+++ webChat/urls.py
@@ -18,10 +18,14 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
     path('chat/', include('chatRoom.urls')),
+    path(
+        "robots.txt/",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
